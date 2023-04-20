ca5285958f66c9bd7d3f1ac8bbae0ebc785ebee8
dhruvdesai2004 <125333958+dhruvdesai2004@users.noreply.github.com>
2023-04-20 16:38:30 +0530

    Add files via upload

---
+++ intro_1 C++/intro_3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+	int i,n[1000],count=0;
+	
+    for(i=2020;i<=3030;i++)
+	{
+	     if(i%4==0)
+		 {
+	         cout<<i << " ";
+		    count++;	 	
+		 }	
+	}	
+	
+}
