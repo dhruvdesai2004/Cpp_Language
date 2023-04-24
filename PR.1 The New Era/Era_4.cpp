deb2205123236c3bb421a7d7e952d0af040a100f
dhruvdesai2004 <125333958+dhruvdesai2004@users.noreply.github.com>
2023-04-24 17:13:17 +0530

    Update Era_4.cpp

--- PR.1 The New Era/Era_4.cpp
+++ PR.1 The New Era/Era_4.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 class detail{
-	int area,n_rooms,flore;
+	int area,n_rooms,floor;
 	
 	public :
 		void input(){
@@ -11,14 +11,14 @@ class detail{
 			cin>>area;
 			cout<<"Enter the number of room in house : ";
 			cin>>n_rooms;
-			cout<<"Enter the number of flore in house : ";
+			cout<<"Enter the number of floor in house : ";
      		cin>>flore;	
 		}
 		
 		void output(){
 			cout<<"\nThe area of house : "<<area;
 			cout<<"\nThe house has given number of rooms : "<<n_rooms;
-			cout<<"\nThe house has given flore of rooms : "<<floor;
+			cout<<"\nThe house has given floor of rooms : "<<floor;
 		}
 };
 
